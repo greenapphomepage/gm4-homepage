6728e3d18955e120ba39956c4b616221b43864d2
Thanh dev <huythanh1308bn@gmail.com>
2024-11-15 22:00:09 +0700

    fix: metadata blog detail

--- app/blog/[id]/layout.tsx
+++ app/blog/[id]/layout.tsx
@@ -1,9 +1,29 @@
-import { Metadata } from "next";
+import {fetcher} from '@/lib/utils'
+import {Blog} from '@/types/Blog'
+import {Metadata} from 'next'
+type MetadataProps = {
+  params: {id: string}
+}
+export async function generateMetadata({
+  params,
+}: MetadataProps): Promise<Metadata> {
+  const {id} = params
+  const result: {data: Blog} = await fetcher(`/blog/${id}`)
+  const detail = result.data
+  return {
+    title: detail.title,
+    description: detail.description,
+    keywords: detail.keywords,
+    alternates: {
+      canonical: `/blog/${id}`,
+    },
+  }
+}
 
 export default function DetailBlogLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return <div className="lg:px-[80px] xl:px-24 pb-11">{children}</div>;
+  return <div className='lg:px-[80px] xl:px-24 pb-11'>{children}</div>
 }
